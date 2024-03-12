a23c60628b65c97e4db0fd0dfba1b1a91e3d0ae6
Diego Lopes <lopesdiego12@gmail.com>
2024-03-12 21:57:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,9 @@
+import pandas as pd
+import numpy as np
 import streamlit as st
 
-st.json({
-    'foo': 'bar',
-    'baz': 'boz',
-    'stuff': [
-        'stuff 1',
-        'stuff 2',
-        'stuff 3',
-        'stuff 5',
-    ],
-})
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
